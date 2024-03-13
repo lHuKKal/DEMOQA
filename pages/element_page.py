7626323f58688d0ab95fdee7cc296b8f4217a75b
HuKKa <1wolfgang555@gmail.com>
2024-03-13 12:36:24 +0500

    First Commit

--- pages/element_page.py
+++ pages/element_page.py
@@ -1,17 +1,17 @@
 import time
 
-from locators.elements_page_locators import TextBoxPageLocators as locators
+from locators.elements_page_locators import TextBoxPageLocators
 from pages.base_page import BasePage
 
 
 class TextBoxPage(BasePage):
-    #   locators = TextBoxPageLocators()
+    locators = TextBoxPageLocators()
 
     def fill_all_fields(self):
-        BasePage.element_is_visible(self, locators.FULL_NAME).send_keys('Full Name SEL')
-        BasePage.element_is_visible(self, locators.EMAIL).send_keys('Exmaple@mail.com')
-        BasePage.element_is_visible(self, locators.CURRENT_ADDRESS).send_keys('Current Address SEL')
-        BasePage.element_is_visible(self, locators.PERMANENT_ADDRESS).send_keys('Permanent Address SEL')
-        BasePage.scroll_to_element(self, locators.SUBMIT)
-        BasePage.element_is_visible(self, locators.SUBMIT).click()
+        BasePage.element_is_visible(self, TextBoxPage.locators.FULL_NAME).send_keys('Full Name SEL')
+        BasePage.element_is_visible(self, TextBoxPage.locators.EMAIL).send_keys('Exmaple@mail.com')
+        BasePage.element_is_visible(self, TextBoxPage.locators.CURRENT_ADDRESS).send_keys('Current Address SEL')
+        BasePage.element_is_visible(self, TextBoxPage.locators.PERMANENT_ADDRESS).send_keys('Permanent Address SEL')
+        BasePage.scroll_to_element(self, TextBoxPage.locators.SUBMIT)
+        BasePage.element_is_visible(self, TextBoxPage.locators.SUBMIT).click()
         time.sleep(5)
