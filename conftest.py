e2e744fdfe47e4f210c407859a5f05054b59df8f
HuKKa <1wolfgang555@gmail.com>
2024-05-13 15:26:49 +0500

    Added allure report to widgets_page.py and widgets_test.py

--- conftest.py
+++ conftest.py
@@ -29,3 +29,18 @@ def pytest_runtest_makereport(item, call):
         attach = driver.get_screenshot_as_png()
         allure.attach(attach, name=f"Screenshot {datetime.today()}", attachment_type=allure.attachment_type.PNG)
 
+
+# Функция сортировку последовательных тестов, если будет необходимо, а лучше сделать маркировку тестов с помощью Pytest
+
+# def pytest_collection_modify_items(config, items):
+#     # Указываем список последовательных тестов
+#     test_order = [
+#         'elements_test',
+#         'forms_test',
+#         'alert_frame_windows_test',
+#         'widgets_test',
+#         'interactions_test'
+#     ]
+#
+#     # Сортировка тестов в соответствии с порядком
+#     items.sort(key=lambda item: test_order.index(item.module.__name__))
--- tests/widgets_test.py
+++ tests/widgets_test.py
@@ -15,9 +15,7 @@ class TestWidgetsSection:
             accordian_page = AccordianPage(driver, "https://demoqa.com/accordian")
             accordian_page.open()
             first_title, first_accordian_text, second_title, second_accordian_text, third_title, third_accordian_text = accordian_page.accordian_page()
-            print(first_title)
-            print(second_title)
-            print(third_title)
+
             assert first_title == "What is Lorem Ipsum?" and first_accordian_text > 0, "First section is not opened or text not exist"
             assert second_title == 'Where does it come from?' and second_accordian_text > 0, "Second section is not opened or text not exist"
             assert third_title == 'Why do we use it?' and third_accordian_text > 0, "Third section is not opened or text not exist"
